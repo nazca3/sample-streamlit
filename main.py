7c17b3d69971ea412af291019c1e5f8a354730d2
nazca3 <97203879+nazca3@users.noreply.github.com>
2022-01-07 09:56:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ if uploaded_file is not None:
         draw.rectangle([(rect['left'],rect['top']),(rect['left']+rect['width'],rect['top']+rect['height'])],fill=None,outline="red",width=5)
         
         font = ImageFont.truetype("Roboto-Regular.ttf", size=size)
-        draw.text((rect['left'],rect['top']-size),gender+str(int(age))),font=font)
+        draw.text((rect['left'],rect['top']-size),gender+str(int(age)),font=font)
         
       
     st.image(img, caption='uploaded Image.', use_column_width=True)
